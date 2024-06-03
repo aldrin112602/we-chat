c57c24f4ee7e7164daded4f5ad6a21d042a3116b
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-06-03 09:49:40 +0800

    Updated commit

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,20 +1,32 @@
 
 const Login = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-200">
-      <form className="shadow-lg p-10 bg-white sm:w-3/4 lg:w-1/2 rounded-md">
-        <h1 className="text-2xl font-semibold text-gray-800">Login</h1>
-        <div className="mt-2">
-          <label htmlFor="uname">Enter username or email</label>
-          <input required type="text" placeholder="Username or email" className="form-input block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="uname" />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 flex-col">
+      <img style={{ filter: 'grayscale(100%)' }} src="/wechat-logo.webp" alt="Wechat logo" className="sm:w-3/4 lg:w-32" />
+      <form className="shadow-lg px-10 pt-1 pb-4 bg-white sm:w-3/4 lg:w-1/3 rounded-md">
+        <h1 className="text-2xl font-semibold text-gray-600 my-5">Sign In</h1>
+        <div className="border my-4 border-1"></div>
+        <div className="mt-4">
+          <label htmlFor="uname">Username</label>
+          <input required type="text" className="form-input block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="uname" />
         </div>
-        <div className="mt-2">
-          <label htmlFor="password">Enter password</label>
-          <input required type="password" placeholder="Password" className="form-input block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="password" />
+        <div className="mt-4">
+          <label htmlFor="password">Password</label>
+          <input required type="password" className="form-input block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="password" />
         </div>
-        <div className="mt-2">
+        <div className="text-right">
+          <a href="#" className="text-gray-600 hover:text-indigo-600">Forgot Password?</a>
+        </div>
+        <div className="mt-4">
           <button className="block w-full rounded-md border-0 py-1.5 px-5 text-slate-100 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset bg-indigo-900 focus:ring-indigo-500 sm:text-sm sm:leading-6">Login</button>
         </div>
+        <div className="border my-4 border-1"></div>
+        <div className="text-center my-3">
+          Sign In with
+        </div>
+        <button type="button" className="block w-full rounded-md border-0 py-1.5 px-5 text-slate-100 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset bg-indigo-600 focus:ring-indigo-500 sm:text-sm sm:leading-6">Facebook</button>
+
+        <button type="button" className="block w-full rounded-md border-0 py-1.5 px-5 text-black mt-2 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset bg-slate-100 focus:ring-indigo-500 sm:text-sm sm:leading-6">Google</button>
       </form>
     </div>
   )
