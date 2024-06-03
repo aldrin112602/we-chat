5bd3ad19bfe8afbcf0601846125c942c4cdf6c2b
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-06-04 07:15:30 +0800

    Updated commit

--- src/pages/Navbar/Navbar.tsx
+++ src/pages/Navbar/Navbar.tsx
@@ -6,22 +6,44 @@ interface FormProps {
 
 const Navbar = ({ isAuth }: FormProps) => {
   const userToken = isAuth();
-  console.log(userToken);
-  
+
   return (
     <>
       <nav className="w-full p-5 px-10 bg-white shadow-lg flex items-center justify-between">
         <h3 className="font-bold text-xl">Wechat</h3>
         <ul className="flex items-center justify-end gap-10">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
+          {/* if user is not Auth */}
+          {!userToken && (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+            </>
+          )}
+
+          {/* if user isAuth */}
+          {userToken && (
+            <>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/friends">Friends</Link>
+              </li>
+              <li>
+                <Link to="/messages">Messages</Link>
+              </li>
+              <li>
+                <Link to="/logout">Logout</Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
       <Outlet />
